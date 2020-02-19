77820cc50047178c6d4e1457784cedd8047a039a
kimich7 <kimich7@gmail.com>
2020-02-19 15:39:46 +0800

    SQL名稱修正

--- php/CMUHconndata.php
+++ php/CMUHconndata.php
@@ -3,7 +3,7 @@
 {
     // 連接資訊 PWS-STATION1
     //$pdo      = new PDO("sqlsrv:Server=PWS-STATION1\PWS;Database=CMUH","sa","ABC!@#456");
-    $pdo      = new PDO("sqlsrv:Server=KIMI-PC\SQL2014;Database=CMUH","sa","ABC!@#456");
+    $pdo      = new PDO("sqlsrv:Server=.\SQLEXPRESS;Database=CMUH","sa","ABC!@#456");
     //$pdo      = new PDO("sqlsrv:Server=10.32.211.16\PWS;Database=CMUH","sa","ABC!@#456");
     //$pdo      = new PDO("sqlsrv:Server=192.168.10.143\SQL2014;Database=CMUH","sa","ABC!@#456");
     $pdo->exec("set names utf8");
