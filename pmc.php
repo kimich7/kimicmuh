bed8289b7f263a28572ddd221e6349d398cff7e9
kimich7 <kimich7@gmail.com>
2020-07-07 14:43:51 +0800

    修正沒有資料時提出警告的標示(讓警告不要顯示)

--- pmc.php
+++ pmc.php
@@ -253,7 +253,7 @@ while ($row=$sql_page->fetch()) {
             $page= isset($_GET['page'])?$_GET['page']:1;        
             @$getpageinfo = page($page,$total_num,$phpfile);
             echo '<div align="center">'; 
-            echo $getpageinfo['pagecode'];//顯示分頁的html語法
+            echo @$getpageinfo['pagecode'];//顯示分頁的html語法
             echo '</div>';
         } 
         
